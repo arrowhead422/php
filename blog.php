971d7c4c44e396d863cdf1d63f1c6840b08b1684
xiang <heroes618@hotmail.com>
2014-11-03 10:37:53 -0800

    video 16 is done

--- blog.php
+++ blog.php
@@ -1,16 +1,14 @@
-<html>
-  <head>
-    <title>Accessing Elements</title>
+
     <link rel="stylesheet" type="text/css" href="bootstrap-theme.min.css">
     <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
     <script type="text/javascript" src="http://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/alphabet.js"></script>
-  </head>
-  <body>
-    <p>
+  
+    
       <?php
 
-
+        require_once(__DIR__ . "/view/header.php");
         require_once(__DIR__ . "/control/create-db.php");
+        require_once(__DIR__ . "/view/footer.php");
         
 
 
@@ -63,7 +61,7 @@
         
        
       ?>
-    </p>
+    
 
 
 
@@ -88,5 +86,4 @@
   
 <script type="text/javaScript" src="bootstrap.min.js"></script>
 <script type="text/javaScript" src="java.js"></script>
-  </body>
-</html>
+  
---
+++ view/header.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Accessing Elements</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap-theme.min.css">
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="http://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/alphabet.js"></script>
+  </head>
+  <body>
