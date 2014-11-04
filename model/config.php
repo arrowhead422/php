96aa7e2a97c0465f501690e9a8f9b637fe2fc9c7
xiang <heroes618@hotmail.com>
2014-11-04 08:54:45 -0800

    video 18 is finish and comment the all

---
+++ model/config.php
@@ -0,0 +1,3 @@
+<?php
+	$path = "/php/";//so this file is a conecter to every file and source 
+?>
--- view/navigation.php
+++ view/navigation.php
@@ -1,7 +1,28 @@
+
+<html>
+
+<head>
+	<title></title>
+
+</head>
+
+<body>
+
+<?php
+	require_once(__DIR__ . "/../model/config.php");// this php file is using the require once to connect the config.php and navigation to display on the website
+
+//
+?>
+
 <nav>
 	<ul>
 		
-		<li><a href="/php/post.php">Blog post Form</a> </li>
+		<li><a href="<?php echo $path . "post.php";?>">Blog post Form</a> </li> 
 	</ul>
 
 </nav>
+
+</body>
+
+
+</html>
