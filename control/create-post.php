a19a4ac6b1c1a12e6a4feb79847fb0dcceaa064f
xiang <heroes618@hotmail.com>
2014-11-12 08:40:25 -0800

    video 23 establishing a database connection

--- control/create-post.php
+++ control/create-post.php
@@ -1,8 +1,15 @@
 <?php
 
+	require_once(__DIR__ . "/../model/database.php");
+
+	$connection = new mysqli($host, $username, $password, $database); // this code connect the database to the create-post.php
+
+	//it recevie information from the post and the title from the the filter input and it also sercer the blogs informations
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-	$post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING);//it recevie information from the post and the title from the the filter input and it also sercer the blogs informations
+	$post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING);
 
-
+	// the title and the post are connect to the form and the post which when it echo it echos out the "title" and "post"
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: $post";
+
+	$connection->close();
