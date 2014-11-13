decf6e86d9517811da3772396961a3c0f10cebf1
xiang <heroes618@hotmail.com>
2014-11-13 09:07:00 -0800

    video 24 queryilng our database to submit a blog post

--- control/create-post.php
+++ control/create-post.php
@@ -3,13 +3,49 @@
 	require_once(__DIR__ . "/../model/database.php");
 
 	$connection = new mysqli($host, $username, $password, $database); // this code connect the database to the create-post.php
+	//it create a new mysqli
 
 	//it recevie information from the post and the title from the the filter input and it also sercer the blogs informations
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING);
 
 	// the title and the post are connect to the form and the post which when it echo it echos out the "title" and "post"
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post";
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post' ");//this query that insers post that set the variable to $title and $post 
+
+	if ($query) {
+		
+		echo "<p>Successfuly inserted post: $title</p>";// if the query is true then it will echo out this echo
+	}
+
+	else {
+
+		echo "<p>$connection->error</p>";// if is not true then it will print out this echo
+
+	}
+
+
 
 	$connection->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- view/header.php
+++ view/header.php
@@ -3,8 +3,10 @@
     <title>Accessing Elements</title>
 
     <header>
+    <p>Q's blog</p>
+    <meta name="viewport" content="width=device-width", "initial-scale=1.0">
     		
-
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 
     </header>
