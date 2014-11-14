dbc116486e97312ee3bab8f5b5d940ed45e74c12
xiang <heroes618@hotmail.com>
2014-11-14 08:57:54 -0800

    video 26 creating a database class

---
+++ model/Database.php
@@ -0,0 +1,10 @@
+<?php// we want to strage the $host $username $password and $Database
+	class Database {
+		private	$connection;// went we access our variable to private it mean is only perform only at Database
+		private $host;
+		private $username;
+		private $password;
+		private $Database;// the class is for the private variable, why because the class can make code alot easyer to read and easyer to use then 
+	}	
+
+
