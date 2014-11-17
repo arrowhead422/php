557b3f057cd0da79e827f9618d8dcbfbc1fb89ca
xiang <heroes618@hotmail.com>
2014-11-17 10:15:53 -0800

    video 27 creating function

--- model/Database.php
+++ model/Database.php
@@ -1,10 +1,17 @@
-<?php// we want to strage the $host $username $password and $Database
+<?php//  this Class are using database for representing as objects, as an object it makes easier to keep database together simpler and easier to understand
 	class Database {
-		private	$connection;// went we access our variable to private it mean is only perform only at Database
+		private	$connection;// went we access our variable to private variable it make the class only perform only at Database
 		private $host;
 		private $username;
 		private $password;
-		private $Database;// the class is for the private variable, why because the class can make code alot easyer to read and easyer to use then 
+		private $Database;// we want to hold the $host $username $password and $Database simple 
+	
+		public function __construct($host, $username, $password, $database){//these variable is called local variable
+			$this->host = $host;// the variable here are connected to the gloal variable
+			$this->username = $username;
+			$this->password = $password;
+			$this->database = $database;
+		}
 	}	
 
 
