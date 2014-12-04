5263f1bce2d2a32b9ab0faf08d3933f99a182cfb
xiang <heroes618@hotmail.com>
2014-12-04 08:51:40 -0800

    datetime ,video 40 and 41

--- control/create-post.php
+++ control/create-post.php
@@ -8,6 +8,8 @@
 	//it recevie information from the post and the title from the the filter input and it also sercer the blogs informations
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING);
+	$date = new DateTime('today');// date variable
+	$time = new DateTime('America/Los_Angeles');//time variable 
 
 	// the title and the post are connect to the form and the post which when it echo it echos out the "title" and "post"
 	$query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post' ");//this query that insers post that set the variable to $title and $post 
@@ -15,6 +17,7 @@
 	if ($query) {
 		
 		echo "<p>Successfuly inserted post: $title</p>";// if the query is true then it will echo out this echo
+		echo "Posted on: " . $date->format("M/D" . "" . "d/Y") . " at " . $time->format("g:i");// it echo out the system of the varible $data and $time
 	}
 
 	else {
