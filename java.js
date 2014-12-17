a26ca0f648ea6e93da4a1029203c1f68de3992b7
xiang <heroes618@hotmail.com>
2014-12-17 07:11:00 -0800

    home.php, title.php are now created with in the php file

--- java.js
+++ java.js
@@ -40,3 +40,42 @@ $('article').readmore({
 $('article').readmore({sectionCSS: 'display: inline-block; width: 50%;'});
 $('article').readmore({embedCSS: false});
 $('article').readmore('destroy');
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  
+  var $slider = $("#navSlider");
+  var $menuItem = $("nav ul li");
+  var $currentPage = $(".current-page");
+  var $currentPagePos = $(".current-page").position();
+  
+  $slider
+      .width("5px")
+      .height($menuItem.height())
+      .css("top", $currentPage.position().top)
+      .data("currentPos", $slider.position().top);
+  
+  $menuItem.hover(function () {
+    var $el = $(this);
+    var $newPos = $el.position().top;
+    
+    $slider.stop().animate({
+      "top": $newPos + "px"
+    }, 200);
+  }, function () {
+      $slider.stop().animate({
+        "top": $slider.data("currentPos")
+      }, 200);
+  });
+});
--- view/header.php
+++ view/header.php
@@ -1,20 +1,27 @@
 <html>
-  <head>
-    <title>blog 101</title>
-      
-       <header>
-  <a href="#" id="logo">
-    <h1>Nick Pettit</h1>
-    <h2>Designer</h2>
-  </a>
-  <nav>
-    <ul>
-      <li><a href="#" class="selected">Portfolio</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-</header>
+<head>
+  
+   <a class="hamburger" href="#" role="button" title="Open menu">
+  <span class="hamburger__bar"></span>
+  <span class="sr-only">Open Menu</span>
+</a>
+</head>
+  <body >
+
+    <nav>
+  <ul>
+    <span id="navSlider"></span>
+    <li><a href="title.php">Title</a></li>
+    <li class="current-page"><a href="home.php">About Me</a></li>
+    <li><a href="contact.html">Click Me</a></li>  
+  </ul>
+</nav>
+
+  
+</div>
+
+ 
+
           <link rel="stylesheet" type="text/css" href="bootstrap-theme.min.css">
           <link rel="stylesheet" type="text/css" href="css/hi.css">
           <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
@@ -34,14 +41,15 @@
 
 
 
-
-  </head>
-<body >
-
-
  <li><a href="/php/register.php">Register</a> </li> 
   <li><a href="/php/login.php">Loging</a> </li> 
 
   <p id="article">A preposition describes a relationship between other words in a sentence. In itself, a word like "in" or "after" is rather meaningless and hard to define in mere words. For instance, when you do try to define a preposition like "in" or "between" or "on," you invariably use your hands to show how something is situated in relationship to something else. Prepositions are nearly always combined with other words in structures called prepositional phrases. Prepositional phrases can be made up of a million different words, but they tend to be built the same: a preposition followed by a determiner and an adjective or two, followed by a pronoun or noun (called the object of the preposition). This whole phrase, in turn, takes on a modifying role, acting as an adjective or an adverb, locating something in time and space, modifying a noun, or telling when or where or under what conditions something happened.</p>
   <a href="javascript:void(0);" class="button">Check it out</a><a href="javascript:void(0);" class="button-readmore"><span>READ MORE</span></a>
 </div>
+
+
+
+
+
+
